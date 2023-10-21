573c8edfdb20500071bbe7dc06576303c11aaeda
Diego Zerbini <zerbiniprogramador@gmail.com>
2023-10-20 21:47:25 -0300

    Criação e inclusão do componente To-Do List

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 window.onload = function(){
+    
     // Background
     const bg = document.querySelector(".bg-img");
 
@@ -54,4 +55,29 @@ window.onload = function(){
         sec.style.transform = `rotateZ(${s * 6}deg)`;
     })
 
+    // Todo-list
+    let inputBox = document.querySelector('#inputBox');
+    let list = document.querySelector('#list');
+
+    inputBox.addEventListener('keyup', function(event){
+    if(event.key == 'Enter') {
+        addItem(this.value)
+        this.value = '';
+    }
+    })
+
+    let addItem = (inputBox) => {
+    let listItem = document.createElement('li');
+    listItem.innerHTML = `${inputBox}<i></i>`;
+
+    listItem.addEventListener("click", function(){
+        this.classList.toggle('done');
+    })
+
+    listItem.querySelector('i').addEventListener('click', function(){
+        listItem.remove();
+    })
+
+    list.appendChild(listItem)
+    }
 }
